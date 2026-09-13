96556b185d9769fc7db565fd5a927ce03e0ee431
daralang <daralangved@gmail.com>
2026-09-13 12:51:03 +0200

    fix: change language to english

--- oving1/printx.c
+++ oving1/printx.c
@@ -4,25 +4,22 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Bruk: %s <antall>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <count>\n", argv[0]);
         return 1;
     }
-
     int x = atoi(argv[1]);
-
     char str[100];
+
     printf("Enter a string: ");
 
     if (fgets(str, sizeof(str), stdin) == NULL) {
-        fprintf(stderr, "Klarte ikke lese fra standard input\n");
+        fprintf(stderr, "Failed to read\n");
         return 1;
     }
-
     str[strcspn(str, "\n")] = '\0';
 
     for (int i = 0; i < x; i++) {
         printf("%s\n", str);
     }
-
     return 0;
 }
